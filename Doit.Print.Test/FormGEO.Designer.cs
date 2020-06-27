271ec9091e695df6d164472fea8dac749ef96ef3
吕中华 <lvzhonghua@foxmail.com>
2020-06-27 23:17:51 +0800

    完善FormGEO

--- Doit.Print.Test/FormGEO.Designer.cs
+++ Doit.Print.Test/FormGEO.Designer.cs
@@ -30,25 +30,20 @@
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.chkWidthFixed = new System.Windows.Forms.CheckBox();
-            this.chkHeightFixed = new System.Windows.Forms.CheckBox();
-            this.tbarWidth = new System.Windows.Forms.TrackBar();
-            this.tbarHeight = new System.Windows.Forms.TrackBar();
-            this.txtWidth = new System.Windows.Forms.TextBox();
-            this.txtHeight = new System.Windows.Forms.TextBox();
-            this.txtContent = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnFont = new System.Windows.Forms.Button();
+            this.lblContentInfo = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.tbarWidth = new System.Windows.Forms.TrackBar();
             this.panContainer = new System.Windows.Forms.Panel();
             this.panGDI = new Doit.Print.Test.PanelGDI();
-            this.lblException = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tbarWidth)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tbarHeight)).BeginInit();
             this.panContainer.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -72,121 +67,76 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.lblException);
-            this.panel1.Controls.Add(this.btnFont);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnFont);
+            this.panel1.Controls.Add(this.lblContentInfo);
             this.panel1.Controls.Add(this.txtContent);
-            this.panel1.Controls.Add(this.txtHeight);
             this.panel1.Controls.Add(this.txtWidth);
-            this.panel1.Controls.Add(this.tbarHeight);
             this.panel1.Controls.Add(this.tbarWidth);
-            this.panel1.Controls.Add(this.chkHeightFixed);
-            this.panel1.Controls.Add(this.chkWidthFixed);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(392, 653);
             this.panel1.TabIndex = 0;
             // 
-            // chkWidthFixed
-            // 
-            this.chkWidthFixed.AutoSize = true;
-            this.chkWidthFixed.Checked = true;
-            this.chkWidthFixed.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkWidthFixed.Location = new System.Drawing.Point(6, 14);
-            this.chkWidthFixed.Name = "chkWidthFixed";
-            this.chkWidthFixed.Size = new System.Drawing.Size(72, 16);
-            this.chkWidthFixed.TabIndex = 0;
-            this.chkWidthFixed.Text = "宽度固定";
-            this.chkWidthFixed.UseVisualStyleBackColor = true;
-            // 
-            // chkHeightFixed
-            // 
-            this.chkHeightFixed.AutoSize = true;
-            this.chkHeightFixed.Location = new System.Drawing.Point(6, 46);
-            this.chkHeightFixed.Name = "chkHeightFixed";
-            this.chkHeightFixed.Size = new System.Drawing.Size(72, 16);
-            this.chkHeightFixed.TabIndex = 0;
-            this.chkHeightFixed.Text = "高度固定";
-            this.chkHeightFixed.UseVisualStyleBackColor = true;
-            // 
-            // tbarWidth
-            // 
-            this.tbarWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbarWidth.AutoSize = false;
-            this.tbarWidth.Location = new System.Drawing.Point(74, 8);
-            this.tbarWidth.Maximum = 250;
-            this.tbarWidth.Name = "tbarWidth";
-            this.tbarWidth.Size = new System.Drawing.Size(273, 22);
-            this.tbarWidth.TabIndex = 1;
-            this.tbarWidth.Value = 100;
-            this.tbarWidth.Scroll += new System.EventHandler(this.tbar_Scroll);
-            // 
-            // tbarHeight
-            // 
-            this.tbarHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbarHeight.AutoSize = false;
-            this.tbarHeight.Location = new System.Drawing.Point(74, 42);
-            this.tbarHeight.Maximum = 250;
-            this.tbarHeight.Name = "tbarHeight";
-            this.tbarHeight.Size = new System.Drawing.Size(273, 22);
-            this.tbarHeight.TabIndex = 1;
-            this.tbarHeight.Value = 50;
-            this.tbarHeight.Scroll += new System.EventHandler(this.tbar_Scroll);
-            // 
-            // txtWidth
-            // 
-            this.txtWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtWidth.Location = new System.Drawing.Point(353, 11);
-            this.txtWidth.Name = "txtWidth";
-            this.txtWidth.Size = new System.Drawing.Size(34, 21);
-            this.txtWidth.TabIndex = 2;
-            this.txtWidth.Text = "400";
-            // 
-            // txtHeight
-            // 
-            this.txtHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtHeight.Location = new System.Drawing.Point(353, 41);
-            this.txtHeight.Name = "txtHeight";
-            this.txtHeight.Size = new System.Drawing.Size(34, 21);
-            this.txtHeight.TabIndex = 2;
-            this.txtHeight.Text = "200";
-            // 
-            // txtContent
-            // 
-            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtContent.Location = new System.Drawing.Point(5, 101);
-            this.txtContent.Multiline = true;
-            this.txtContent.Name = "txtContent";
-            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtContent.Size = new System.Drawing.Size(382, 520);
-            this.txtContent.TabIndex = 3;
-            this.txtContent.Text = "生命中有太多对你重要的东西，如果不往内心深处去探索，这些东西就都会变成杂音。\r\n\r\n如何保持持续的专注呢？\r\n1、要在一个漫长的赛道上\r\n2、要有持续不断的心力和" +
-    "茁壮的体力\r\n3、不断更新迭代的创始人认知，同频更新的高管团队，以及创始人的管理能力\r\n4、最关键的“不被卡住”的那点运气，柳暗花明又一村的感觉";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 12);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "要显示的内容：";
-            // 
             // btnFont
             // 
             this.btnFont.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFont.Location = new System.Drawing.Point(220, 72);
+            this.btnFont.Location = new System.Drawing.Point(220, 32);
             this.btnFont.Name = "btnFont";
             this.btnFont.Size = new System.Drawing.Size(167, 23);
             this.btnFont.TabIndex = 5;
             this.btnFont.Text = "字体(宋体,12)";
             this.btnFont.UseVisualStyleBackColor = true;
             this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // lblContentInfo
+            // 
+            this.lblContentInfo.AutoSize = true;
+            this.lblContentInfo.Location = new System.Drawing.Point(4, 36);
+            this.lblContentInfo.Name = "lblContentInfo";
+            this.lblContentInfo.Size = new System.Drawing.Size(89, 12);
+            this.lblContentInfo.TabIndex = 4;
+            this.lblContentInfo.Text = "要显示的内容：";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Location = new System.Drawing.Point(5, 61);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(382, 589);
+            this.txtContent.TabIndex = 3;
+            this.txtContent.Text = "生命中有太多对你重要的东西，如果不往内心深处去探索，这些东西就都会变成杂音。\r\n\r\n如何保持持续的专注呢？\r\n1、要在一个漫长的赛道上\r\n2、要有持续不断的心力和" +
+    "茁壮的体力\r\n3、不断更新迭代的创始人认知，同频更新的高管团队，以及创始人的管理能力\r\n4、最关键的“不被卡住”的那点运气，柳暗花明又一村的感觉";
+            this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWidth.Location = new System.Drawing.Point(353, 5);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.ReadOnly = true;
+            this.txtWidth.Size = new System.Drawing.Size(34, 21);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.Text = "400";
+            // 
+            // tbarWidth
+            // 
+            this.tbarWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbarWidth.AutoSize = false;
+            this.tbarWidth.Location = new System.Drawing.Point(42, 2);
+            this.tbarWidth.Maximum = 250;
+            this.tbarWidth.Minimum = 1;
+            this.tbarWidth.Name = "tbarWidth";
+            this.tbarWidth.Size = new System.Drawing.Size(305, 22);
+            this.tbarWidth.TabIndex = 1;
+            this.tbarWidth.Value = 100;
+            this.tbarWidth.Scroll += new System.EventHandler(this.tbar_Scroll);
             // 
             // panContainer
             // 
@@ -208,16 +158,14 @@
             this.panGDI.TabIndex = 2;
             this.panGDI.Paint += new System.Windows.Forms.PaintEventHandler(this.panGDI_Paint);
             // 
-            // lblException
+            // label1
             // 
-            this.lblException.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.lblException.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lblException.ForeColor = System.Drawing.Color.Red;
-            this.lblException.Location = new System.Drawing.Point(0, 628);
-            this.lblException.Name = "lblException";
-            this.lblException.Size = new System.Drawing.Size(392, 25);
-            this.lblException.TabIndex = 6;
-            this.lblException.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "宽度：";
             // 
             // FormGEO
             // 
@@ -235,7 +183,6 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tbarWidth)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tbarHeight)).EndInit();
             this.panContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -245,17 +192,13 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TrackBar tbarHeight;
         private System.Windows.Forms.TrackBar tbarWidth;
-        private System.Windows.Forms.CheckBox chkHeightFixed;
-        private System.Windows.Forms.CheckBox chkWidthFixed;
         private System.Windows.Forms.Button btnFont;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContentInfo;
         private System.Windows.Forms.TextBox txtContent;
-        private System.Windows.Forms.TextBox txtHeight;
         private System.Windows.Forms.TextBox txtWidth;
         private System.Windows.Forms.Panel panContainer;
         private PanelGDI panGDI;
-        private System.Windows.Forms.Label lblException;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Doit.Print.Test/FormGEO.cs
+++ Doit.Print.Test/FormGEO.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Doit.Print.Test
@@ -13,6 +7,8 @@ namespace Doit.Print.Test
     public partial class FormGEO : Form
     {
         private Font currentFont = new Font("宋体",12f);
+        private SizeF sizeOfContent = SizeF.Empty;
+
         public FormGEO()
         {
             InitializeComponent();
@@ -28,6 +24,8 @@ namespace Doit.Print.Test
             this.currentFont = fontDlg.Font;
             this.txtContent.Font = this.currentFont;
             this.btnFont.Text = $"字体({this.currentFont.Name},{this.currentFont.Size},{this.currentFont.Style})";
+
+            this.panGDI.Refresh();
         }
 
         private void FormGEO_Load(object sender, EventArgs e)
@@ -41,18 +39,35 @@ namespace Doit.Print.Test
         private void DrawContent(Graphics graphics)
         {  
             int width = int.Parse(this.txtWidth.Text.Trim());
-            int height = int.Parse(this.txtHeight.Text.Trim());
 
             int x = 40;
             int y = 40;
 
+            //绘制外框
+            string content = this.txtContent.Text.Trim();
+            int height = (int)Doit.UI.GEOHelper.GetContentHeightOnFixedWidth(graphics, content, this.currentFont, width);
+            Rectangle rectContent = new Rectangle(x,y,width, (int)height);
+            Pen penOfRectangle = new Pen(Color.Gray,1f);
+            penOfRectangle.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            graphics.DrawRectangle(penOfRectangle, rectContent);
+            //绘制文字
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Near;
+            format.LineAlignment = StringAlignment.Center;
+            format.Trimming = StringTrimming.Character;
+            graphics.DrawString(content, this.currentFont, Brushes.Gray, rectContent,format);
+            
+            this.sizeOfContent = graphics.MeasureString(content, this.currentFont,width);
+            this.lblContentInfo.Text = $"要显示的内容：(w:{this.sizeOfContent.Width} h:{this.sizeOfContent.Height})";
+
+            //绘制宽度标识
             Point A = Point.Empty;
             Point B = Point.Empty;
 
             Pen penOfSize = new Pen(Color.Blue, 1f);
-            //绘制宽度标识
-            A = new Point(x,y - 25);
-            B = new Point(x,y - 5);
+
+            A = new Point(x, y - 25);
+            B = new Point(x, y - 5);
             graphics.DrawLine(penOfSize, A, B);
 
             A = new Point(x + width, y - 25);
@@ -64,7 +79,7 @@ namespace Doit.Print.Test
             B = new Point(x + width, y - 15);
             graphics.DrawLine(penOfSize, A, B);
 
-            Font font = new Font("宋体",12f);
+            Font font = new Font("宋体", 12f);
             int spacing = 4;
             SizeF sizeOfWidthValue = graphics.MeasureString(width.ToString(), font);
             RectangleF rectOfWidthValue = new RectangleF();
@@ -72,7 +87,7 @@ namespace Doit.Print.Test
             rectOfWidthValue.Height = sizeOfWidthValue.Height + spacing * 2;
             rectOfWidthValue.X = x + (width - rectOfWidthValue.Width) / 2;
             rectOfWidthValue.Y = y - spacing - rectOfWidthValue.Height;
-            Doit.UI.GDIHelper.FillRoundRectangleWithText(graphics, rectOfWidthValue, width.ToString(), font, Color.Blue, Color.White, spacing, spacing,5, true);
+            Doit.UI.GDIHelper.FillRoundRectangleWithText(graphics, rectOfWidthValue, width.ToString(), font, Color.Blue, Color.White, spacing, spacing, 5, true);
 
             //绘制高度标识
             penOfSize.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
@@ -93,31 +108,55 @@ namespace Doit.Print.Test
             RectangleF rectOfHeightValue = new RectangleF();
             rectOfHeightValue.Width = sizeOfHeightValue.Width + spacing * 2;
             rectOfHeightValue.Height = sizeOfHeightValue.Height + spacing * 2;
-            rectOfHeightValue.X =  (height - rectOfHeightValue.Width) / 2;
+            rectOfHeightValue.X = (height - rectOfHeightValue.Width) / 2;
             rectOfHeightValue.Y = y - spacing - rectOfHeightValue.Height;
 
             graphics.TranslateTransform(0, y + height);
             graphics.RotateTransform(-90f);
             Doit.UI.GDIHelper.FillRoundRectangleWithText(graphics, rectOfHeightValue, height.ToString(), font, Color.Blue, Color.White, spacing, spacing, 5, true);
             graphics.RotateTransform(90f);
-            graphics.TranslateTransform(-0, - (y + height));
+            graphics.TranslateTransform(-0, -(y + height));
 
-            //绘制外框
-            Rectangle rectContent = new Rectangle(x,y,width,height);
-            Pen penOfRectangle = new Pen(Color.Gray,1f);
-            penOfRectangle.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
-            graphics.DrawRectangle(penOfRectangle, rectContent);
+            //绘制箭头
+            penOfSize.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            A = new Point(x, y - 15);
+            B = new Point(x + 5, y - 10);
+            graphics.DrawLine(penOfSize, A, B);
+
+            B = new Point(x + 5, y - 20);
+            graphics.DrawLine(penOfSize, A, B);
+
+            A = new Point(x + width , y - 15);
+            B = new Point(x + width - 5, y - 10);
+            graphics.DrawLine(penOfSize, A, B);
+
+            B = new Point(x + width - 5, y - 20);
+            graphics.DrawLine(penOfSize, A, B);
+
+            A = new Point(x - 15, y);
+            B = new Point(x - 10, y + 5);
+            graphics.DrawLine(penOfSize, A, B);
+
+            B = new Point(x - 20, y + 5);
+            graphics.DrawLine(penOfSize, A, B);
+
+            A = new Point(x - 15, y + height);
+            B = new Point(x - 10, y + height - 5);
+            graphics.DrawLine(penOfSize, A, B);
+
+            B = new Point(x - 20, y + height - 5);
+            graphics.DrawLine(penOfSize, A, B);
+
+
+            this.panGDI.Height = (this.panContainer.Height >= (20 + height + 20)) ? this.panContainer.Height : (20 + height + 20);
         }
 
         private void tbar_Scroll(object sender, EventArgs e)
         {
-            this.lblException.Text = "";
-
             this.txtWidth.Text = (this.tbarWidth.Value * 5).ToString();
-            this.txtHeight.Text = (this.tbarHeight.Value * 5).ToString();
 
             int width = int.Parse(this.txtWidth.Text.Trim());
-            int height = int.Parse(this.txtHeight.Text.Trim());
+            int height = int.Parse(this.txtWidth.Text.Trim());
 
             this.panGDI.Width = (this.panContainer.Width >= (20 + width + 20)) ? this.panContainer.Width : (20 + width + 20);
             this.panGDI.Height = (this.panContainer.Height >= (20 + height + 20)) ? this.panContainer.Height : (20 + height + 20);
@@ -128,6 +167,11 @@ namespace Doit.Print.Test
         private void panGDI_Paint(object sender, PaintEventArgs e)
         {
             this.DrawContent(e.Graphics);
+        }
+
+        private void txtContent_TextChanged(object sender, EventArgs e)
+        {
+            this.panGDI.Refresh();
         }
     }
 }
